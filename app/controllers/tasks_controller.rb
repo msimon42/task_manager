cf429cd791bb4197dc933325665e50393dfaac82
msimon42 <msimon@uab.edu>
2019-11-14 19:55:07 -0700

    Complete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,41 @@
 class TasksController < ApplicationController
   def index
-    @tasks = ['Sweep floor', 'Wash Dishes', 'Eat lunch']
+    @tasks = Task.all
   end
-end     
+
+  def new
+
+  end
+
+  def create
+    task = Task.new({
+      title: params[:task][:title],
+      description: params[:task][:description]
+    })
+    task.save
+    redirect_to '/tasks'
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update({
+      title:params[:task][:title],
+      description: params[:task][:description]
+    })
+    task.save
+    redirect_to "/tasks/#{task.id}"
+  end
+
+  def destroy
+    Task.destroy(params[:id])
+    redirect_to '/tasks'
+  end
+end
